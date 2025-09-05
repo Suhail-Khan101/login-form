4cfddc657ee571ba7774688fdb31e8e80628ef50
Suhail Khane <179018041+Suhail-Khan101@users.noreply.github.com>
2025-09-05 13:55:28 +0100

    fix: static file cache headers and ZAP ignore warnings (-I)

--- login_form/__init__.py
+++ login_form/__init__.py
@@ -42,10 +42,19 @@ def create_app(test_config=None):
         # Remove Server header to prevent version disclosure
         if 'Server' in resp.headers:
             del resp.headers['Server']
-        # Cache control headers for all responses
-        resp.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
-        resp.headers['Pragma'] = 'no-cache'
-        resp.headers['Expires'] = '0'
+        # Set cache headers for static files
+        if resp.status_code == 200 and resp.direct_passthrough is False and hasattr(resp, 'headers') and hasattr(resp, 'mimetype') and resp.mimetype and resp.mimetype.startswith(('text/css', 'image/', 'font/', 'application/javascript')):
+            resp.headers['Cache-Control'] = 'public, max-age=31536000, immutable'
+            resp.headers.pop('Pragma', None)
+            resp.headers.pop('Expires', None)
+        elif '/static/' in getattr(resp, 'location', '') or (hasattr(resp, 'request') and getattr(resp.request, 'path', '').startswith('/static/')):
+            resp.headers['Cache-Control'] = 'public, max-age=31536000, immutable'
+            resp.headers.pop('Pragma', None)
+            resp.headers.pop('Expires', None)
+        else:
+            resp.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+            resp.headers['Pragma'] = 'no-cache'
+            resp.headers['Expires'] = '0'
         # Other security headers
         resp.headers['X-Frame-Options'] = 'DENY'
         resp.headers['X-Content-Type-Options'] = 'nosniff'
@@ -100,12 +109,20 @@ def create_app(test_config=None):
     # 404 error handler with cache-control
     @app.errorhandler(404)
     def not_found_error(error):
-        from flask import make_response
-        return make_response('Not Found', 404)
+    from flask import make_response
+    response = make_response('Not Found', 404)
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response
 
     # 400 error handler with cache-control
     @app.errorhandler(400)
     def bad_request_error(error):
-        from flask import make_response
-        return make_response('Bad Request', 400)
+    from flask import make_response
+    response = make_response('Bad Request', 400)
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response
     return app 
